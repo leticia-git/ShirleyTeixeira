4efec45f6435c34647ca51baeb199011f705602b
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-03 17:45:45 -0300

    Update index.php

--- wp-content/themes/Shirley/index.php
+++ wp-content/themes/Shirley/index.php
@@ -4,14 +4,27 @@ Template Name: Home
 */
 get_header(); ?>
 
-
 <section class="um">
     <div class="grid">
         <div class="projetos">
-            <div class="projetos titulo">
-                <p>PROJETOS</p>
+            <div class="textos-projeto">
+                <div class="projetos-titulo">
+                    <p>PROJETOS</p>
+                </div>
+                <div class="projetos-texto">
+                    <p>Navegue pelo meu portfólio criativo Navegue pelo meu portfólio
+                        criativoNavegue pelo meu portfólio criativoNavegue pelo meu portfólio
+                        criativoNavegue pelo meu portfólio criativoNavegue pelo meu portfólio
+                        criativoNavegue pelo meu portfólio criativo</p>
+                </div>
+            </div>
+            <div class="topicos-projetos">
+                <div class="topico">Blog</div>
+                <div class="topico">Newsletter</div>
+                <div class="topico">Redes sociais</div>
             </div>
         </div>
+
     </div>
 </section>
 
