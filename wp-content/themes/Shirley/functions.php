f6ccbbee7a11147060cbd1dd26084879b245d491
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-03 15:59:49 -0300

    Update functions.php

--- wp-content/themes/Shirley/functions.php
+++ wp-content/themes/Shirley/functions.php
@@ -25,7 +25,7 @@ if ( ! function_exists( 'twentytwentytwo_support' ) ) :
 		add_theme_support( 'wp-block-styles' );
 
 		// Enqueue editor styles.
-		add_editor_style( 'app.css' );
+		add_editor_style( 'style.css' );
 	}
 
 endif;
@@ -48,7 +48,7 @@ if ( ! function_exists( 'twentytwentytwo_styles' ) ) :
 		$version_string = is_string( $theme_version ) ? $theme_version : false;
 		wp_register_style(
 			'twentytwentytwo-style',
-			get_template_directory_uri() . '/app.css',
+			get_template_directory_uri() . '/style.css',
 			array(),
 			$version_string
 		);
