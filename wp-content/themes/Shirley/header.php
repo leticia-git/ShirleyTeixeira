f64593b1eb4ef643c8f3f6f6e151ae86e68f07de
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-03 15:53:10 -0300

    Update header.php

--- wp-content/themes/Shirley/header.php
+++ wp-content/themes/Shirley/header.php
@@ -24,7 +24,7 @@
 	<!-- CSS only -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
 	<link rel="stylesheet" href="css/font-awesome.min.css">
-	<link rel="stylesheet" href="app.css">
+	<link rel="stylesheet" href="css/app.css">
 	<?php wp_head(); ?>
 </head>
 
