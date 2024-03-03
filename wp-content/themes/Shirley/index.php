d2dd013194e08659e63c66345cf5af6da61a8bf8
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-03 17:41:26 -0300

    Criando section de projetos

--- wp-content/themes/Shirley/index.php
+++ wp-content/themes/Shirley/index.php
@@ -1,5 +1,18 @@
-<?php get_header(); ?>
+<?php
+/*
+Template Name: Home
+*/
+get_header(); ?>
 
 
+<section class="um">
+    <div class="grid">
+        <div class="projetos">
+            <div class="projetos titulo">
+                <p>PROJETOS</p>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
