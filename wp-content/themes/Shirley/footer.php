add89778a8446b4a0a0671e28bbe57a5305ad589
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-08 20:29:27 -0300

    Criando footer 💅🏻 💅🏻

--- wp-content/themes/Shirley/footer.php
+++ wp-content/themes/Shirley/footer.php
@@ -1,6 +1,6 @@
 </body>
 
-<footer style="background-image:url('<?php echo home_url(); ?>/wp-content/themes/Shirley/assets/images/fundo-footer.png')">
+<footer style="background-image:url('<?php echo home_url(); ?>/wp-content/themes/Shirley/assets/images/fundo-footer.png');background-repeat:no-repeat; background-size:cover">
     <div class="grid">
         <div class="logo-footer">
             <img src="<?php echo home_url(); ?>/wp-content/themes/Shirley/assets/images/logofooter.png" alt="Logo">
