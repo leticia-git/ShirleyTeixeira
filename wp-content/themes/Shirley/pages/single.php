47feadd77ee4e47eb04035a4cdf2290fb5c92d34
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-13 20:50:02 -0300

    Criando Blog 💅🏻 💅🏻

--- wp-content/themes/Shirley/pages/single.php
+++ wp-content/themes/Shirley/pages/single.php
@@ -19,11 +19,6 @@ get_template_part('components/head'); ?>
                     <h1 class="p-single__title" ><?php the_title(); ?></h1>
                     <span class="p-single__subtitle" ></span>
                 </div>
-                <div class="p-single__author-date" >
-                    <span class="p-single__author" ><?php the_author(); ?></span>
-                    <span >-</span>
-                    <span class="p-single__data" ><?php echo get_the_date('d'); ?> <?php echo get_the_date('F'); ?> <?php echo get_the_date('Y'); ?></span>
-                </div>
             </div>
         <?php } ?>
         <div class="p-single__content-latest" >
