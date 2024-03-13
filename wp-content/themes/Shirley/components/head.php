7471026e676459696c6e19765faf23a93118d6dd
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-13 20:48:13 -0300

    Criando Blog 💅🏻 💅🏻

---
+++ wp-content/themes/Shirley/components/head.php
@@ -0,0 +1,29 @@
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IT=edge,chrome=IE8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta property="og:type" content="website" />
+	<meta property="og:locale" content="pt_BR" />
+	<meta name="theme-color" content="#000" />
+	<meta name="msapplication-navbutton-color" content="#000" />
+	<meta name="apple-mobile-web-app-status-bar-style" content="#000" />
+	<title>
+		<?php if (is_front_page()) {
+			echo bloginfo('name');
+		} else {
+			echo the_title();
+			echo ' - ';
+			echo bloginfo('name');
+		} ?>
+	</title>
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Fascinate&display=swap" rel="stylesheet">
+
+	<!-- CSS only -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+	<link rel="stylesheet" href="<?php echo home_url(); ?>/wp-content/themes/Shirley/assets/css/app.css">
+	<?php wp_head(); ?>
+</head>
--- wp-content/themes/Shirley/pages/single.php
+++ wp-content/themes/Shirley/pages/single.php
@@ -1,18 +1,19 @@
+
+
+
 <main class="p-single">
     <div class="p-single__grid" >
         <?php if (has_post_thumbnail()) { ?>
-            <div class="p-single__thumbnail swiper-slide" style="background-image: url('<?php the_post_thumbnail_url('thumbnail-blog-highlights') ?>')" >
+            <div class="p-single__thumbnail swiper-slide" >
                 <div class="p-single__titles" >
                     <h1 class="p-single__title" ><?php the_title(); ?></h1>
                 </div>
                 <div class="p-single__author-date" >
                     <span class="p-single__author" ><?php the_author(); ?></span>
-                    <span >-</span>
-                    <span class="p-single__data" ><?php echo get_the_date('d'); ?> <?php echo get_the_date('F'); ?> <?php echo get_the_date('Y'); ?></span>
                 </div>
             </div>
         <?php } else { ?>
-            <div class="p-single__thumbnail swiper-slide" style="background-color: #00306A" >
+            <div class="p-single__thumbnail swiper-slide" >
                 <div class="p-single__titles" >
                     <h1 class="p-single__title" ><?php the_title(); ?></h1>
                     <span class="p-single__subtitle" ></span>
