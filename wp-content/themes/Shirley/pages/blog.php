9ad44bd9748baf6bd17aeafaa66d5b37577834e9
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-14 21:20:22 -0300

    Update blog.php

--- wp-content/themes/Shirley/pages/blog.php
+++ wp-content/themes/Shirley/pages/blog.php
@@ -9,46 +9,48 @@ Template Name:Blog
     <?php get_template_part('components/head'); ?>
     <?php wp_head(); ?>
 
-        <div class="grid">
+ <div class="blog">
+ <div class="grid">
             <?php get_template_part('templates/blog-menu'); ?>
-        </div>
+    </div>
 
     <div class="conteudo-blog">
-    <div class="grid">
-        <main>
-            <section class="blog-posts">
-                <div class="container">
-                    <?php
-                    $args = array(
-                        'post_type'      => 'post',
-                        'posts_per_page' => -1, // Mostra todos os posts
-                    );
+        <div class="grid">
+            <main>
+                <section class="blog-posts">
+                    <div class="container">
+                        <?php
+                        $args = array(
+                            'post_type'      => 'post',
+                            'posts_per_page' => -1, // Mostra todos os posts
+                        );
 
-                    $query = new WP_Query($args);
+                        $query = new WP_Query($args);
 
-                    if ($query->have_posts()) :
-                        while ($query->have_posts()) :
-                            $query->the_post();
-                    ?>
-                            <article>
-                                <div class="post-thumbnail">
-                                    <?php the_post_thumbnail(); ?>
-                                </div>
-                                <h2><?php the_title(); ?></h2>
-                                <p><?php the_excerpt(); ?></p>
-                                <a href="<?php the_permalink(); ?>">Leia mais</a>
-                            </article>
-                    <?php
-                        endwhile;
-                        wp_reset_postdata();
-                    else :
-                        echo "<p>Nenhum post encontrado.</p>";
-                    endif;
-                    ?>
-                </div>
-            </section>
-        </main>
+                        if ($query->have_posts()) :
+                            while ($query->have_posts()) :
+                                $query->the_post();
+                        ?>
+                                <article>
+                                    <div class="post-thumbnail">
+                                        <?php the_post_thumbnail(); ?>
+                                    </div>
+                                    <h2><?php the_title(); ?></h2>
+                                    <p><?php the_excerpt(); ?></p>
+                                    <a href="<?php the_permalink(); ?>">Leia mais</a>
+                                </article>
+                        <?php
+                            endwhile;
+                            wp_reset_postdata();
+                        else :
+                            echo "<p>Nenhum post encontrado.</p>";
+                        endif;
+                        ?>
+                    </div>
+                </section>
+            </main>
+        </div>
     </div>
-</div>
+ </div>
 
 <?php get_footer(); ?>
