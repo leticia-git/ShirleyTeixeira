82fd9974eb1d713941601dbae9a79110c02cf85b
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-17 17:54:27 -0300

    Ajustando pagina interna blog 💅🏻 💅🏻

--- wp-content/themes/Shirley/pages/single.php
+++ wp-content/themes/Shirley/pages/single.php
@@ -1,28 +1,40 @@
 <?php 
 get_template_part('components/head'); ?>
 
+<?php get_template_part('components/head'); ?>
+<?php wp_head(); ?>
 
-<main class="p-single">
-    <div class="grid" >
-        <?php if (has_post_thumbnail()) { ?>
-            <div class="p-single__thumbnail" >
-                <div class="p-single__titles" >
-                    <h1 class="p-single__title" ><?php the_title(); ?></h1>
-                </div>
-                <div class="p-single__author-date" >
-                    <span class="p-single__author" ><?php the_author(); ?></span>
-                </div>
-            </div>
-        <?php } else { ?>
-            <div class="p-single__thumbnail" >
-                <div class="p-single__titles" >
-                    <h1 class="p-single__title" ><?php the_title(); ?></h1>
-                    <span class="p-single__subtitle" ></span>
-                </div>
-            </div>
-        <?php } ?>
-        <div class="p-single__content-latest" >
-            <div class="p-single__content" ><?php the_content() ?></div>
+    <div class="single">
+        <div class="grid">
+                <?php get_template_part('templates/blog-menu'); ?>
         </div>
+        <div class="tag-blog">
+                <img src="<?php echo home_url(); ?>/wp-content/themes/Shirley/assets/images/blog-tag.png" alt="Blog">
+        </div>
+        <main class="p-single">
+            <div class="grid" >
+                <?php if (has_post_thumbnail()) { ?>
+                    <div class="p-single__thumbnail" >
+                        <div class="p-single__titles" >
+                            <h1 class="p-single__title" ><?php the_title(); ?></h1>
+                        </div>
+                    </div>
+                <?php } else { ?>
+                    <div class="p-single__thumbnail" >
+                        <div class="p-single__titles" >
+                            <h1 class="p-single__title" ><?php the_title(); ?></h1>
+                            <span class="p-single__subtitle" ></span>
+                        </div>
+                    </div>
+                <?php } ?>
+                <div class="post-thumbnail">
+                    <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
+                </div>
+                <div class="p-single__content-latest" >
+                    <div class="p-single__content" ><?php the_content() ?></div>
+                </div>
+            </div>
+        </main>
     </div>
-</main>
+
+<?php get_footer(); ?>
