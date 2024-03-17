2756af24fb08bd83ce99b7f56ecfc16776cf1de5
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-17 17:37:23 -0300

    Ajustando Blog 💅🏻 💅🏻

--- wp-content/themes/Shirley/pages/blog.php
+++ wp-content/themes/Shirley/pages/blog.php
@@ -6,93 +6,66 @@ Template Name:Blog
 
 
 
-    <?php get_template_part('components/head'); ?>
-    <?php wp_head(); ?>
+<?php get_template_part('components/head'); ?>
+<?php wp_head(); ?>
 
- <div class="blog">
- <div class="grid">
-            <?php get_template_part('templates/blog-menu'); ?>
-    </div>
-
-    <div class="conteudo-blog">
-        <div class="tag-blog">
-        <img
-            src="<?php echo home_url(); ?>/wp-content/themes/Shirley/assets/images/blog-tag.png"
-            alt="Blog">
-        </div>
+    <div class="blog">
         <div class="grid">
-        <div id="category-filter">
-    <ul>
-        <?php
-        // Get all categories
-        $categories = get_categories();
+                <?php get_template_part('templates/blog-menu'); ?>
+        </div>
 
-        // Display each category as a link
-        foreach ($categories as $category) {
-            echo '<li><a href="#" data-category="' . $category->slug . '">' . $category->name . '</a></li>';
-        }
-        ?>
-    </ul>
-</div>
-            <main>
-                <section class="blog-posts">
-                    <div class="container">
-                        <?php
-                        $args = array(
-                            'post_type'      => 'post',
-                            'posts_per_page' => -1, // Mostra todos os posts
-                        );
-
-                        $query = new WP_Query($args);
-
-                        if ($query->have_posts()) :
-                            while ($query->have_posts()) :
-                                $query->the_post();
-                        ?>
-                                <article>
-                                    <div class="post-thumbnail">
-                                        <?php the_post_thumbnail(); ?>
-                                    </div>
-                                    <div class="titulo-post">
-                                        <h2><?php the_title(); ?></h2>
-                                    </div>
-                                    <div class="resumo">
-                                        <p><?php the_excerpt(); ?></p>
-                                    </div>
-                                    <div class="leiamais">
-                                        <a href="<?php the_permalink(); ?>">Leia mais</a>
-                                    </div>
-                                </article>
-                        <?php
-                            endwhile;
-                            wp_reset_postdata();
-                        else :
-                            echo "<p>Nenhum post encontrado.</p>";
-                        endif;
-                        ?>
+        <div class="conteudo-blog">
+            <div class="tag-blog">
+                <img src="<?php echo home_url(); ?>/wp-content/themes/Shirley/assets/images/blog-tag.png" alt="Blog">
+            </div>
+            <div class="grid">
+                <main>
+                    <div class="titulo-posts">
+                        <p>POSTS</p>
+                        <img src="<?php echo home_url(); ?>/wp-content/themes/Shirley/assets/images/florbranca.png" alt="Flor">
                     </div>
-                </section>
-            </main>
+                    <section class="blog-posts">
+                        <div class="container">
+                            <?php
+                            $args = array(
+                                'post_type'      => 'post',
+                                'posts_per_page' => -1, // Mostra todos os posts
+                            );
+
+                            $query = new WP_Query($args);
+
+                            if ($query->have_posts()) :
+                                while ($query->have_posts()) :
+                                    $query->the_post();
+                            ?>
+                                    <article>
+                                        <div class="post-thumbnail">
+                                            <?php the_post_thumbnail(); ?>
+                                        </div>
+                                        <div class="titulo-post">
+                                            <h2><?php the_title(); ?></h2>
+                                        </div>
+                                        <div class="resumo">
+                                            <p><?php the_excerpt(); ?></p>
+                                        </div>
+                                        <div class="leiamais">
+                                            <a href="<?php the_permalink(); ?>">Leia mais</a>
+                                        </div>
+                                    </article>
+                            <?php
+                                endwhile;
+                                wp_reset_postdata();
+                            else :
+                                echo "<p>Nenhum post encontrado.</p>";
+                            endif;
+                            ?>
+                        </div>
+                    </section>
+                </main>
+            </div>
         </div>
     </div>
- </div>
+  
 <script>
-jQuery(document).ready(function($) {
-    $('#category-filter a').on('click', function(e) {
-        e.preventDefault();
 
-        var category = $(this).data('category');
-
-        // Hide all posts
-        $('.post').hide();
-
-        // Show only the posts in the selected category
-        if (category === 'all') {
-            $('.post').show();
-        } else {
-            $('.' + category).show();
-        }
-    });
-});
-</script>
 <?php get_footer(); ?>
