63784e4d1552dd7fe963ace5c7d580697f19e66f
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-17 18:24:11 -0300

    Ajustando menu

--- wp-content/themes/Shirley/header.php
+++ wp-content/themes/Shirley/header.php
@@ -14,13 +14,13 @@ get_template_part('components/head'); ?>
 				</div>
 				<div class="conteudo-menu">
 					<div class="links">
-						<a href="#">HOME</a>
+						<a href="<?php echo home_url(); ?>/">HOME</a>
 						<i class="fa-solid fa-star" style="color: #DD90AE;"></i>
-						<a href="#projetos">PROJETOS</a>
+						<a href="<?php echo home_url(); ?>/projetos">PROJETOS</a>
 						<i class="fa-solid fa-star" style="color: #DD90AE;"></i>
-						<a href="#sobre">SOBRE</a>
+						<a href="<?php echo home_url(); ?>/sobre">SOBRE</a>
 						<i class="fa-solid fa-star" style="color: #DD90AE;"></i>
-						<a href="#contato">CONTATO</a>
+						<a href="<?php echo home_url(); ?>/contato">CONTATO</a>
 					</div>
 					<div class="redes">
 						<div class="rede">
