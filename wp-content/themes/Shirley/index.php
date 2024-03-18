f88bbe425001e621ec9639e0434e306267b3673a
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-18 20:32:08 -0300

    Ajuste

--- wp-content/themes/Shirley/index.php
+++ wp-content/themes/Shirley/index.php
@@ -75,7 +75,7 @@
                     <img src="<?php echo home_url(); ?>/wp-content/themes/Shirley/assets/images/flores.png" alt="">
                 </div>
                 <div class="botao-cv">
-                    <a href="https://shirleyteixeira.com.br/Downloads/Shirley_Teixeira_da_Silva.pdf" download>
+                    <a href="https://shirleyteixeira.com.br/wp-content/uploads/2024/03/Shirley-Teixeira-da-Silva-CV.pdf" download>
                     <i class="fa-solid fa-star" style="color: #fff;"></i>
                     BAIXE MEU CV
                     <i class="fa-solid fa-star" style="color: #fff;"></i>
