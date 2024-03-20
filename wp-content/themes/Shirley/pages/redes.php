c54f64659a31c61d6c2d6aba589ff4c861a2440e
Leticia Araújo <71797111+leticia-git@users.noreply.github.com>
2024-03-20 09:45:17 -0300

    Ajuste

---
+++ wp-content/themes/Shirley/pages/redes.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Template Name:Redes 
+*/?>
+
+<?php get_template_part('components/head'); ?>
+<?php wp_head(); ?>
+
+        <div class="grid">
+                <?php get_template_part('templates/menu-blog'); ?>
+        </div>
+            <div class="tag-blog">
+                <img src="<?php echo home_url(); ?>/wp-content/themes/Shirley/assets/images/blog-tag.png" alt="Blog">
+            </div>
